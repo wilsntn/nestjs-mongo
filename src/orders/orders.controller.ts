6e2cdf3711c3e5e0c37596ca33b2f3949815169b
wilsntn <wilcson.denner@gmail.com>
2022-07-26 17:32:11 -0400

    @changes in order controller

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -3,14 +3,12 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
   Request,
 } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
-import { UpdateOrderDto } from './dto/update-order.dto';
 
 @Controller('orders')
 export class OrdersController {
